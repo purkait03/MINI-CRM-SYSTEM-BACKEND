add5e53f10183bc437f3134594557601fe2cb5e3
purkait03 <purkaitsougata@gmail.com>
2026-01-21 02:33:02 +0530

    feat: add reminder model and integrate nodemailer and twilio for communication

---
+++ src/models/reminder.model.js
@@ -0,0 +1,40 @@
+import { Schema } from "mongoose";
+import mongoose from "mongoose";
+
+const reminder = new Schema({
+    clientId: {
+        type: mongoose.Schema.Types.ObjectId,
+        Ref: "Client",
+        require: true
+    },
+
+    channel: {
+        type: String,
+        enum: ["Email", "SMS"],
+        require: true
+    },
+
+    message: {
+        type: String,
+        require: true
+    },
+
+    remindAt:{
+        type: Date,
+        default: Date.now
+    },
+
+    status:{
+        type: String,
+        enum: ["PENDING", "SENT"],
+        default: "PENDING"
+    },
+
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        require: true
+    }
+})
+
+export const Reminder = mongoose.model("Reminder", reminder)
